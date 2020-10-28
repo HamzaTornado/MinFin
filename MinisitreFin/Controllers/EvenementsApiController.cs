23ac5405f690483040b8c6a1b759d2719f50488e
HamzaTornado <hamzaze17@gmail.com>
2020-10-28 15:04:02 +0100

    Api update

--- MinisitreFin/Controllers/EvenementsApiController.cs
+++ MinisitreFin/Controllers/EvenementsApiController.cs
@@ -10,7 +10,6 @@ using MinisitreFin.Models;
 using MinisitreFin.ViewModels;
 using System.Web.Http.Cors;
 using System.Web.Http.Description;
-
 namespace MinisitreFin.Controllers
 {
     [EnableCors(origins: "http://localhost:4401", headers: "*", methods: "get") ]
@@ -21,72 +20,101 @@ namespace MinisitreFin.Controllers
         [HttpGet]
         public IHttpActionResult GetEvenements()
         {
-            var events = db.Evenements.ToList();
-            
-            return Ok(events);
+            try
+            {
+                var events = db.Evenements.ToList();
+                List<EvenementsList> evn = new List<EvenementsList>();
+                foreach (var even in events )
+                {
+                    EvenementsList evenements = new EvenementsList
+                    {
+                        ID = even.ID,
+                        Titre_even = even.Titre_even,
+                        Description = even.Description,
+                        Date_debut = even.Date_debut.Value,
+                        Date_fin = even.Date_fin.Value
+                    };
+                    evn.Add(evenements);
+                }
+                return Ok(evn);
+            }
+            catch (Exception)
+            {
+                //mail to UGP@Soukattanmia.ma 
+                return BadRequest();
+            } 
             
         }
-
-        //[ResponseType(typeof(Evenements))]
-        //public IHttpActionResult GetEvenements(int id)
-        //{
-        //    Evenements evenements = db.Evenements.Find(id);
-        //    if (evenements == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return Ok(evenements);
-        //}
-
-        // GET: api/EvenementsApi
-        //[HttpGet]
-        //public IEnumerable Get()
-        //{
-        //    List<EvenementsList> Eventlist = new List<EvenementsList>();
-        //    var events = db.Evenements.Where(e=>e.Statut==true).ToList();
-        //    if (events == null)
-        //    {
-        //        yield return NotFound();
-        //    }
-        //    var request = HttpContext.Current.Request;
-        //    var appUrl = HttpRuntime.AppDomainAppVirtualPath;
-        //    if (appUrl != "/")
-        //        appUrl = "/" + appUrl;
-        //    var baseUrl = string.Format("{0}://{1}{2}", request.Url.Scheme, request.Url.Authority, appUrl);
-
-        //    foreach (var even in events)
-        //    {
-        //        EvenementsList evn = new EvenementsList() {
-        //            ID = even.ID,
-        //            Titre_even = even.Titre_even,
-        //            Description = even.Description,
-        //            Date_debut = even.Date_debut.Value,
-        //            Date_fin = even.Date_fin.Value
-        //        };
-        //        Eventlist.Add(evn);
-        //    }
-        //    yield return db.Evenements.Where(e => e.Statut == true).ToArray();
-        //}
-
         // GET: api/EvenementsApi/5
         [HttpGet]
         public IHttpActionResult Get(int id)
         {
-            var even = db.Evenements.Find(id);
-            if (even == null)
+            try
             {
-                return NotFound();
+                var even = db.Evenements.Find(id);
+                if (even == null)
+                {
+                    return NotFound();
+                }
+                EvenementsList evn = new EvenementsList()
+                {
+                    ID = even.ID,
+                    Titre_even = even.Titre_even,
+                    Description = even.Description,
+                    Date_debut = even.Date_debut.Value,
+                    Date_fin = even.Date_fin.Value
+                };
+                return Ok(evn);
             }
-            EvenementsList evn = new EvenementsList()
+            catch(Exception)
             {
-                ID = even.ID,
-                Titre_even = even.Titre_even,
-                Description = even.Description,
-                Date_debut = even.Date_debut.Value,
-                Date_fin = even.Date_fin.Value
-            };
-            return Ok(evn);
+                //mail to UGP@Soukattanmia.ma 
+                return BadRequest();
+            }
+            
         }
     }
 }
+
+
+//[ResponseType(typeof(Evenements))]
+//public IHttpActionResult GetEvenements(int id)
+//{
+//    Evenements evenements = db.Evenements.Find(id);
+//    if (evenements == null)
+//    {
+//        return NotFound();
+//    }
+
+//    return Ok(evenements);
+//}
+
+// GET: api/EvenementsApi
+//[HttpGet]
+//public IEnumerable Get()
+//{
+//    List<EvenementsList> Eventlist = new List<EvenementsList>();
+//    var events = db.Evenements.Where(e=>e.Statut==true).ToList();
+//    if (events == null)
+//    {
+//        yield return NotFound();
+//    }
+//    var request = HttpContext.Current.Request;
+//    var appUrl = HttpRuntime.AppDomainAppVirtualPath;
+//    if (appUrl != "/")
+//        appUrl = "/" + appUrl;
+//    var baseUrl = string.Format("{0}://{1}{2}", request.Url.Scheme, request.Url.Authority, appUrl);
+
+//    foreach (var even in events)
+//    {
+//        EvenementsList evn = new EvenementsList() {
+//            ID = even.ID,
+//            Titre_even = even.Titre_even,
+//            Description = even.Description,
+//            Date_debut = even.Date_debut.Value,
+//            Date_fin = even.Date_fin.Value
+//        };
+//        Eventlist.Add(evn);
+//    }
+//    yield return db.Evenements.Where(e => e.Statut == true).ToArray();
+//}
